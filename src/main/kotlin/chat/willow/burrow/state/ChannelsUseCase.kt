6d9d5bc9aa5bfb2e3968321ea50a571a591873d8
Sky Welch <git@bunnies.io>
2017-12-10 21:01:24 +0000

    Starting to implement PART logic

--- src/main/kotlin/chat/willow/burrow/state/ChannelsUseCase.kt
+++ src/main/kotlin/chat/willow/burrow/state/ChannelsUseCase.kt
@@ -2,11 +2,12 @@ package chat.willow.burrow.state
 
 import chat.willow.burrow.Burrow
 import chat.willow.burrow.connection.IConnectionTracker
+import chat.willow.burrow.helper.loggerFor
 import chat.willow.kale.KaleObservable
 import chat.willow.kale.helper.CaseInsensitiveNamedMap
-import chat.willow.kale.helper.CaseMapping
 import chat.willow.kale.helper.INamed
 import chat.willow.kale.irc.message.rfc1459.JoinMessage
+import chat.willow.kale.irc.message.rfc1459.PartMessage
 
 interface IChannelsUseCase {
 
@@ -19,12 +20,18 @@ data class Channel(override val name: String): INamed
 
 class ChannelsUseCase(private val connections: IConnectionTracker): IChannelsUseCase {
 
+    private val LOGGER = loggerFor<ChannelsUseCase>()
+
     override val channels = CaseInsensitiveNamedMap<Channel>(mapper = Burrow.Server.MAPPER)
 
     override fun track(client: ClientTracker.ConnectedClient) {
         client.kale
                 .observe(JoinMessage.Command.Descriptor)
                 .subscribe { handleJoin(it, client) }
+
+        client.kale
+                .observe(PartMessage.Command.Descriptor)
+                .subscribe { handlePart(it, client) }
     }
 
     private fun handleJoin(observable: KaleObservable<JoinMessage.Command>, client: ClientTracker.ConnectedClient) {
@@ -38,6 +45,20 @@ class ChannelsUseCase(private val connections: IConnectionTracker): IChannelsUse
             }
 
             val message = JoinMessage.Message(source = client.prefix, channels = observable.message.channels)
+            connections.send(client.connection.id, message)
+        }
+    }
+
+    private fun handlePart(observable: KaleObservable<PartMessage.Command>, client: ClientTracker.ConnectedClient) {
+        observable.message.channels.forEach {
+            val channel = channels[it]
+            if (channel == null) {
+                LOGGER.warn("client left a nonexistent channel $it $client")
+            } else {
+                channels -= channel.name
+            }
+
+            val message = PartMessage.Message(source = client.prefix, channels = observable.message.channels)
             connections.send(client.connection.id, message)
         }
     }
--- src/test/kotlin/chat/willow/burrow/unit/state/ChannelsUseCaseTests.kt
+++ src/test/kotlin/chat/willow/burrow/unit/state/ChannelsUseCaseTests.kt
@@ -6,11 +6,11 @@ import chat.willow.burrow.state.ChannelsUseCase
 import chat.willow.burrow.utility.KaleUtilities
 import chat.willow.kale.IKale
 import chat.willow.kale.irc.message.rfc1459.JoinMessage
+import chat.willow.kale.irc.message.rfc1459.PartMessage
 import com.nhaarman.mockito_kotlin.mock
 import com.nhaarman.mockito_kotlin.verify
 import io.reactivex.subjects.PublishSubject
-import junit.framework.Assert.assertEquals
-import junit.framework.Assert.assertTrue
+import org.junit.Assert.*
 import org.junit.Before
 import org.junit.Test
 
@@ -22,12 +22,14 @@ class ChannelsUseCaseTests {
     private lateinit var mockKale: IKale
 
     private lateinit var joins: PublishSubject<JoinMessage.Command>
+    private lateinit var parts: PublishSubject<PartMessage.Command>
 
     @Before fun setUp() {
         mockConnections = mock()
         mockKale = KaleUtilities.mockKale()
 
         joins = mockKaleObservable(mockKale, JoinMessage.Command.Descriptor)
+        parts = mockKaleObservable(mockKale, PartMessage.Command.Descriptor)
 
         sut = ChannelsUseCase(mockConnections)
     }
@@ -59,6 +61,25 @@ class ChannelsUseCaseTests {
         assertTrue(sut.channels.contains("new_channel"))
         assertTrue(sut.channels.contains("New_Channel"))
         assertEquals(1, sut.channels.all.keys.count())
+    }
+
+    @Test fun `when a client leaves a channel, it is destroyed`() {
+        val client = makeClient(mockKale)
+        sut.track(client)
+        joins.onNext(JoinMessage.Command(channels = listOf("existing_channel")))
+
+        parts.onNext(PartMessage.Command(channels = listOf("existing_channel")))
+
+        assertFalse(sut.channels.contains("existing_channel"))
+    }
+
+    @Test fun `when a client leaves a channel, they get a PART reply`() {
+        val client = makeClient(mockKale)
+        sut.track(client)
+
+        parts.onNext(PartMessage.Command(channels = listOf("existing_channel")))
+
+        verify(mockConnections).send(id = 1, message = PartMessage.Message(source = client.prefix, channels = listOf("existing_channel")))
     }
 
 }
