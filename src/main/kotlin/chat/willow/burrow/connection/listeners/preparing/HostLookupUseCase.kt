64a07df67874d6514c3f6467ca8f62856c5fec8c
Sky Welch <git@bunnies.io>
2018-01-17 15:39:59 +0000

    Make sure host lookup gets done on the right scheduler (subscribeOn vs observeOn)

--- src/main/kotlin/chat/willow/burrow/connection/listeners/preparing/HostLookupUseCase.kt
+++ src/main/kotlin/chat/willow/burrow/connection/listeners/preparing/HostLookupUseCase.kt
@@ -13,7 +13,7 @@ interface IHostLookupUseCase {
 
 }
 
-class HostLookupUseCase(val timerScheduler: Scheduler = Schedulers.io()): IHostLookupUseCase {
+class HostLookupUseCase(val lookupScheduler: Scheduler = Schedulers.io(), val timerScheduler: Scheduler = Schedulers.io()): IHostLookupUseCase {
 
     private val LOGGER = loggerFor<HostLookupUseCase>()
 
@@ -22,7 +22,6 @@ class HostLookupUseCase(val timerScheduler: Scheduler = Schedulers.io()): IHostL
     object ForwardLookupMistmatch: RuntimeException()
 
     override fun lookUp(address: InetAddress, default: String): Observable<String> {
-        LOGGER.info("Looking up hostname: $address")
         val hostname = reverseLookupHostname(address)
 
         val resolvedHostname = hostname
@@ -43,6 +42,7 @@ class HostLookupUseCase(val timerScheduler: Scheduler = Schedulers.io()): IHostL
             }
 
         return hostnameChecks
+            .subscribeOn(lookupScheduler)
             .map { it.first }
             .timeout(HOSTNAME_LOOKUP_TIMEOUT_SECONDS, TimeUnit.SECONDS, timerScheduler)
             .onErrorReturnItem(default)
@@ -50,6 +50,7 @@ class HostLookupUseCase(val timerScheduler: Scheduler = Schedulers.io()): IHostL
 
     private fun reverseLookupHostname(address: InetAddress): Observable<String> {
         return Observable.fromCallable {
+            LOGGER.info("Looking up hostname: $address")
             canonicalHostName(address)
         }
     }
