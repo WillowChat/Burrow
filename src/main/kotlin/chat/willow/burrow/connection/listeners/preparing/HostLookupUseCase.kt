f7af240f3448b78535fe8d52184772b8d6510b3d
Sky Welch <git@bunnies.io>
2018-01-17 15:33:00 +0000

    Do a forward lookup of hostnames after reversing to verify them

--- src/main/kotlin/chat/willow/burrow/connection/listeners/preparing/HostLookupUseCase.kt
+++ src/main/kotlin/chat/willow/burrow/connection/listeners/preparing/HostLookupUseCase.kt
@@ -19,19 +19,44 @@ class HostLookupUseCase(val timerScheduler: Scheduler = Schedulers.io()): IHostL
 
     private val HOSTNAME_LOOKUP_TIMEOUT_SECONDS: Long = 10
 
+    object ForwardLookupMistmatch: RuntimeException()
+
     override fun lookUp(address: InetAddress, default: String): Observable<String> {
         LOGGER.info("Looking up hostname: $address")
-        val hostname = tryResolveHostname(address)
+        val hostname = reverseLookupHostname(address)
 
-        return hostname
+        val resolvedHostname = hostname
+            .doOnError { LOGGER.warn("Hostname reverse lookup failed: $address") }
+
+        val hostnameChecks = resolvedHostname
+            .flatMap(
+                { forwardLookupHostnameMatches(it, address) },
+                { it: String, validated: Boolean -> it to validated })
+            .flatMap {
+                if (it.second) {
+                    LOGGER.info("Forward lookup verified hostname ${address.hostAddress} maps to ${it.first}")
+                    Observable.just(it)
+                } else {
+                    LOGGER.warn("Forward lookup mismatch $address ${it.first}")
+                    Observable.error(ForwardLookupMistmatch)
+                }
+            }
+
+        return hostnameChecks
+            .map { it.first }
             .timeout(HOSTNAME_LOOKUP_TIMEOUT_SECONDS, TimeUnit.SECONDS, timerScheduler)
-            .doOnError { LOGGER.warn("Hostname lookup failed: $address") }
             .onErrorReturnItem(default)
     }
 
-    private fun tryResolveHostname(address: InetAddress): Observable<String> {
+    private fun reverseLookupHostname(address: InetAddress): Observable<String> {
         return Observable.fromCallable {
             canonicalHostName(address)
+        }
+    }
+
+    private fun forwardLookupHostnameMatches(host: String, original: InetAddress): Observable<Boolean> {
+        return Observable.fromCallable {
+            InetAddress.getByName(host).hostAddress == original.hostAddress
         }
     }
 
